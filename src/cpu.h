6965ed45277626329ee046b286dfd33e0f090fd9
Reece <rmctiernan1@gmail.com>
2018-05-25 22:41:50 +0100

    Refactor set/clear flag functions to one respectively

---
+++ src/cpu.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct Cpu {
+	uint8_t a;                      //Accumulator register
+	uint8_t f;                      //Flag register (lower 4 bits are always 0)
+	uint8_t b, c, d, e, h, l;       //8 bit registers 
+	uint16_t sp, pc;               //16 bit registers
+	uint8_t m, t;                  //clocks for last instruction
+								   //t increments with each clock step, m being a quarter of t
+} Cpu;
+
+enum flag {
+	CARRY_FLAG = 0x10,				/* Last operation result was over 255 (addition)
+									or under 0 (subtraction) */
+	HALFCARRY_FLAG = 0x20,			/* Last operation's result's lower half of
+									byte overflowed past 15 */
+	SUBTRACTION_FLAG = 0x40,		//Last operation was a subtraction
+	ZERO_FLAG = 0x40				//Last operation had result of 0
+};
+
+void set_flag(Cpu* cpu, int flag); 
+void clear_flag(Cpu* cpu, int flag);
+
+void add_a_8bit_constant(Cpu* cpu, uint8_t n);
+void add_a_a(Cpu* cpu);
+void add_a_b(Cpu* cpu);
+void add_a_c(Cpu* cpu);
+void add_a_d(Cpu* cpu);
+void add_a_e(Cpu* cpu);
+void add_a_h(Cpu* cpu);
+void add_a_l(Cpu* cpu);
+
